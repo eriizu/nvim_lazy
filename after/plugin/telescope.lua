6541ddb8f5c1502670b830cfd62a58d7506c2346
Élise <elise@eiss.fr>
2023-02-22 16:39:09 +0100

    changed binding on C-t to search all files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-t>', builtin.git_files, {})
+vim.keymap.set('n', '<C-t>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("find string: ") });
 end)
