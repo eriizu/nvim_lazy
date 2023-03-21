c6691614b2b31e5ee4a29f40025adc9695bd1a5c
Élise <elise@eiss.fr>
2023-03-21 22:04:52 +0100

    stop showing mode since it is in lua bard

--- lua/eriizu/opt.lua
+++ lua/eriizu/opt.lua
@@ -7,4 +7,5 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = "tab:»-,nbsp:+,trail:·,extends:→,precedes:←"
 vim.opt.wildignore = ".git,.hg,.svn,*.pyc,*.o,*.jpg,*.jpeg,*.png,*.gif,*.zip,*.DS_Store,**/node_modules/**,**/bower_modules/**"
+vim.opt.showmode = false
 vim.g.mapleader = " "
