28abf27543d1f8f73eeb233db2e454c3fd774f36
Élise <elise@eiss.fr>
2023-03-26 21:49:16 +0200

    minor config changes

--- lua/eriizu/plugins/lsp-zero.lua
+++ lua/eriizu/plugins/lsp-zero.lua
@@ -1,54 +1,67 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
 
-lsp.preset('recommended')
+lsp.preset("recommended")
 
 -- (Optional) Configure lua language server for neovim
 -- lsp.nvim_workspace()
 
-lsp.configure('lua_ls', {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+lsp.configure("lua_ls", {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
 })
 
 lsp.setup_nvim_cmp({
-  preselect = 'none',
-  completion = {
-    completeopt = 'menu,menuone,noinsert,noselect'
-  },
+    preselect = "none",
+    completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+    },
+    sources = {
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "luasnip", keyword_length = 2 },
+    },
 })
 
-lsp.skip_server_setup({'rust_analyzer'})
+lsp.skip_server_setup({ "rust_analyzer" })
 
 lsp.setup()
 
 -- Initialize rust_analyzer with rust-tools
-local rust_lsp = lsp.build_options('rust_analyzer', {})
-require('rust-tools').setup({server = rust_lsp})
+local rust_lsp = lsp.build_options("rust_analyzer", {})
+require("rust-tools").setup({
+    server = rust_lsp,
+    tools = {
+        inlay_hints = {
+            --            parameter_hints_prefix = "param: ",
+            --            other_hints_prefix = "type: ",
+        },
+    },
+})
 
-require "lsp_signature".setup({
+require("lsp_signature").setup({
     hint_prefix = "sig: ",
     hint_enable = false,
     doc_lines = 0,
     floating_window_off_x = 0,
     handler_opts = {
-        border = "rounded"
-    }
+        border = "rounded",
+    },
 })
