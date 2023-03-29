cb544afc5168f7cbc35b76f36c1073130baace9c
Élise <elise@eiss.fr>
2023-03-29 17:05:37 +0200

    fixed rust debug config and added neovide specific settings

--- init.lua
+++ init.lua
@@ -11,3 +11,15 @@ for _, name in ipairs(core_conf_files) do
     local source_cmd = "source " .. path
     vim.cmd(source_cmd)
 end
+
+if vim.g.neovide then
+    vim.o.guifont = "Fira Mono:h10"
+    vim.g.neovide_padding_top = 5
+    vim.g.neovide_padding_bottom = 5
+    vim.g.neovide_padding_right = 5
+    vim.g.neovide_padding_left = 5
+    vim.g.neovide_transparency = 0.9
+    vim.g.neovide_floating_blur_amount_x = 2.0
+    vim.g.neovide_floating_blur_amount_y = 2.0
+end
+
--- lua/eriizu/plugins/lsp-zero.lua
+++ lua/eriizu/plugins/lsp-zero.lua
@@ -48,13 +48,21 @@ lsp.setup()
 
 -- Initialize rust_analyzer with rust-tools
 local rust_lsp = lsp.build_options("rust_analyzer", {})
+local extension_path = vim.env.HOME .. "/.local/share/nvim/codelldb/extension/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+print(codelldb_path)
 require("rust-tools").setup({
     server = rust_lsp,
     tools = {
         inlay_hints = {
-            --            parameter_hints_prefix = "param: ",
-            --            other_hints_prefix = "type: ",
+            -- max_len_align = true,
+                        parameter_hints_prefix = " <- ",
+                        other_hints_prefix = " => ",
         },
+    },
+    dap = {
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
     },
 })
 
