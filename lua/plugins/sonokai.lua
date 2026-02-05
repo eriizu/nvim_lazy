7109abd58c35a3e452e525664bb943bb8739aca1
Élise <elise@eiss.fr>
2026-02-05 19:11:26 +0100

    moved back to sonokai

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -1,34 +1,51 @@
 return {
-	{
-		"eriizu/gruvbox-material",
-		branch = "master",
-		lazy = false,
-		enabled = true,
-		config = function()
-			vim.g.gruvbox_material_background = "medium"
-			vim.g.gruvbox_material_foreground = "material"
-			if not vim.g.neovide then
-				vim.g.gruvbox_material_transparent_background = 1
-			end
-			vim.g.gruvbox_material_better_performance = 1
-			vim.g.gruvbox_material_dim_inactive_windows = 0
-			vim.g.gruvbox_material_enable_italic = 1
-			vim.g.gruvbox_material_enable_bold = 0
-			vim.opt.termguicolors = true
-			vim.cmd([[colorscheme gruvbox-material]])
-		end
-	},
-	{
-		"sainnhe/sonokai",
-		lazy = false,
-		enabled = false,
-		config = function()
-			vim.g.sonokai_style = "espresso"
-			vim.g.sonokai_transparent_background = 2
-			vim.g.sonokai_better_performance = 1
-			vim.g.sonokai_dim_inactive_windows = 0
-			vim.opt.termguicolors = true
-			vim.cmd([[colorscheme sonokai]])
-		end
-	}
+    -- {
+    --     "sainnhe/edge",
+    --     lazy = false,
+    --     config = function()
+    --         vim.g.edge_material_background = "medium"
+    --         vim.g.edge_material_foreground = "material"
+    --         if not vim.g.neovide then
+    --             vim.g.edge_material_transparent_background = 1
+    --         end
+    --         vim.g.edge_material_better_performance = 1
+    --         vim.g.edge_material_dim_inactive_windows = 0
+    --         vim.g.edge_material_enable_italic = 1
+    --         vim.g.edge_material_enable_bold = 0
+    --         vim.opt.termguicolors = true
+    --         vim.cmd([[colorscheme edge]])
+    --     end
+    -- },
+    -- {
+    --     "eriizu/gruvbox-material",
+    --     branch = "master",
+    --     lazy = false,
+    --     enabled = false,
+    --     config = function()
+    --         vim.g.gruvbox_material_background = "medium"
+    --         vim.g.gruvbox_material_foreground = "material"
+    --         if not vim.g.neovide then
+    --             vim.g.gruvbox_material_transparent_background = 1
+    --         end
+    --         vim.g.gruvbox_material_better_performance = 1
+    --         vim.g.gruvbox_material_dim_inactive_windows = 0
+    --         vim.g.gruvbox_material_enable_italic = 1
+    --         vim.g.gruvbox_material_enable_bold = 0
+    --         vim.opt.termguicolors = true
+    --         vim.cmd([[colorscheme gruvbox-material]])
+    --     end
+    -- },
+    {
+        "sainnhe/sonokai",
+        lazy = false,
+        enabled = true,
+        config = function()
+            vim.g.sonokai_style = "shusia"
+            vim.g.sonokai_transparent_background = 0
+            vim.g.sonokai_better_performance = 1
+            vim.g.sonokai_dim_inactive_windows = 0
+            vim.opt.termguicolors = true
+            vim.cmd([[colorscheme sonokai]])
+        end
+    }
 }
